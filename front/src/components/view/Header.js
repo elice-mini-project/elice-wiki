d30cc47972c68bb6f2ed9723396837b48ad8b4ed
Kim-Hyunseo <kimhyunseo305@gmail.com>
2022-05-28 15:17:44 +0900

    feat : app, header 충돌 해결

--- front/src/components/view/Header.js
+++ front/src/components/view/Header.js
@@ -44,7 +44,6 @@ function Header() {
     return navigate("/auth");
   };
 
-<<<<<<< HEAD
   return (
     <>
       <Box sx={{ flexGrow: 1 }} style={{ width: "100%", height: "60px" }}>
@@ -81,44 +80,22 @@ function Header() {
                 onClick={handleProfileMenuOpen}
                 color="inherit"
               >
-                <AccountCircle />
+                <img
+                  alt="Profile"
+                  src={userState?.profile_img}
+                  style={{ width: "24px", height: "24px", borderRadius: "50%" }}
+                />
               </IconButton>
-=======
-                        <Box sx={{ display: "flex" }}>
-                            <IconButton
-                                size="large"
-                                edge="end"
-                                aria-label="account of current user"
-                                aria-controls={menuId}
-                                aria-haspopup="true"
-                                onClick={handleProfileMenuOpen}
-                                color="inherit"
-                            >
-                                <img
-                                    src={userState?.profile_img}
-                                    style={{ width: "24px", height: "24px", borderRadius: "50%" }}
-                                />
-                            </IconButton>
-                        </Box>
-                    </Toolbar>
-                </AppBar>
-                <Menu anchorEl={anchorEl} id={menuId} open={isMenuOpen} onClose={handleMenuClose}>
-                    <MenuItem onClick={handleClick}>My Page</MenuItem>
-                    {(userState?.admin === 0 || userState?.admin === 1) && (
-                        <MenuItem color="inherit" onClick={() => navigate("/admin/users")}>
-                            Admin
-                        </MenuItem>
-                    )}
-                    <hr />
-                    <MenuItem onClick={handleLogout}>Logout</MenuItem>
-                </Menu>
->>>>>>> 3525079a597457e8b4dc3e1e5be301c8a16f8b46
             </Box>
           </Toolbar>
         </AppBar>
         <Menu anchorEl={anchorEl} id={menuId} open={isMenuOpen} onClose={handleMenuClose}>
           <MenuItem onClick={handleClick}>My Page</MenuItem>
-          <MenuItem>My Account</MenuItem>
+          {(userState?.admin === 0 || userState?.admin === 1) && (
+            <MenuItem color="inherit" onClick={() => navigate("/admin/users")}>
+              Admin
+            </MenuItem>
+          )}
           <hr />
           <MenuItem onClick={handleLogout}>Logout</MenuItem>
         </Menu>
